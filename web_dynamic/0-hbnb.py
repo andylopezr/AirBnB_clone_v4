c535d4b22634127416d77cacc723410e4fcb7d65
Jong9106 <juank_hr@hotmail.com>
2021-05-13 20:58:56 -0500

    ADDED | uuid library

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
-from flask import Flask, render_template
+from uuid import uuid4
+
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -34,11 +37,11 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=uuid.uuid4())
+                           cache_id=uuid4())
 
 
 if __name__ == "__main__":
