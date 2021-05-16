24d96e0d6723af8e257fe59294c54f9b5f50fffb
Andy Lopez <profesor.de.ingles@aol.com>
2021-05-16 09:12:06 -0500

    UPDATED | styles and js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,17 @@
-  $(function () {
-    const amenities = {};
-  
-    $('input:checkbox').change(
-      function () {
-        if ($(this).is(':checked')) {
-          amenities[($(this).attr('data-id'))] = $(this).attr('data-name');
-        } else {
-          delete amenities[($(this).attr('data-id'))];
-        }
-        $('div.amenities h4').html(Object.values(amenities).join(', ') || '&nbsp;');
-      });
+$(document).ready(function () {
+  const amenities = {};
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    let tex = '';
+    if (!(Object.values(amenities).length)) {
+      $('.amenities h4').replaceWith('<h4>&nbsp;</h4>');
+    } else {
+      tex = Object.values(amenities).join(', ');
+      $('.amenities h4').html(tex);
+    }
   });
+});
